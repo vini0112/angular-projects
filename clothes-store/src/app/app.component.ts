c21a9383daf0e94a51aa4b77a90007cdd7bf6913
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-05-29 11:45:32 -0300

    footer done

--- clothes-store/src/app/app.component.ts
+++ clothes-store/src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit, PLATFORM_ID, signal } from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './user_page/navbar/navbar.component';
 import { ProductsService } from '../services/products.service';
 import { MessageComponent } from './message/message.component';
 import { AuthLoginService } from '../services/auth.login.service';
 import { FooterComponent } from './user_page/footer/footer.component';
+import { isPlatformBrowser } from '@angular/common';
 
 
 
@@ -20,15 +21,26 @@ export class AppComponent implements OnInit{
 
   productService = inject(ProductsService)
   authLoginService = inject(AuthLoginService)
+  plataformId: object = inject(PLATFORM_ID)
 
 
-  constructor() { }
+  constructor(private router: Router) {
+
+    this.router.events.subscribe(event =>{
+      if(event instanceof NavigationEnd && isPlatformBrowser(this.plataformId)){
+        window.scrollTo(0, 0)
+      }
+    })
+
+  }
+
 
   ngOnInit(): void {
     this.authLoginService.checkIfIsLogged()
     this.productService.getProducts()
   }
-  
+
+
 
   
 }
--- clothes-store/src/app/user_page/footer/footer.component.ts
+++ clothes-store/src/app/user_page/footer/footer.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, Inject, inject, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
+import { AuthLoginService } from '../../../services/auth.login.service';
+import { AsyncPipe, isPlatformBrowser, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [RouterLink, AsyncPipe, NgIf],  
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css'
 })
 export class FooterComponent {
 
+  authLoginService = inject(AuthLoginService)
+  // plataformId = inject(PLATFORM_ID)
+
   emailForContact = 'viniciuseloi10@gmail.com'
+
+  isAuth$ = this.authLoginService.isAuthenticated$
+
+  // constructor(private router: Router, @Inject(PLATFORM_ID) private plataformId: Object){
+
+  //   this.router.events.subscribe(event =>{
+  //     if(event instanceof NavigationEnd && isPlatformBrowser(this.plataformId)){
+  //         window.scrollTo(0, 0)
+  //     }
+  //   })
+
+  // }
+
+
+
 
 }

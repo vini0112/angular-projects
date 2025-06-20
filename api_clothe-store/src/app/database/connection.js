a07e454211668c8182ef503c44d98276c15e96cd
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-06-20 08:50:44 -0300

    configure database readme

--- api_clothe-store/src/app/database/connection.js
+++ api_clothe-store/src/app/database/connection.js
@@ -12,14 +12,6 @@ const connection = mysql.createConnection({
 
 
 
-/**
- * 
- * @param {string} sql instrução sql a ser executada
- * @param {string=id | [clothes, id]} valores a serem passados para o sql
- * @param {string} mensagemReject msg de erro
- * @returns obj da primise
- */
-
 export const consulta = (sql, valores='', mensagemReject) =>{
 
     return new Promise((resolve, reject) =>{

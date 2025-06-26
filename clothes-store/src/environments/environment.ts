4ce069862d241944e853840b9126d50e191deffb
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-06-26 13:31:13 -0300

    Update environment.ts

--- clothes-store/src/environments/environment.ts
+++ clothes-store/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    api: 'http://www.localhost:3000',
+    production: true,
+    api: 'http://backend-production-9982.up.railway.app',
     stripe_public_key:"pk_test_51R9xxPBVdE5HzU7bXUGt6dXnr5V0oknWCu7ZqRdhGejhnhFwcnIe9YEVuAABaOpE7rV4sMBU903AL9JPmTvZ30ro00HVN8OM6p",
 
 };

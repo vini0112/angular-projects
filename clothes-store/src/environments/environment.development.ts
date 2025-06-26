d2a595a35c10156a7e1f9f7d0b8420a72aab45f7
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-06-26 10:43:32 -0300

    .

--- clothes-store/src/environments/environment.development.ts
+++ clothes-store/src/environments/environment.development.ts
@@ -2,9 +2,4 @@ export const environment = {
     api: 'http://www.localhost:3000',
     stripe_public_key:"pk_test_51R9xxPBVdE5HzU7bXUGt6dXnr5V0oknWCu7ZqRdhGejhnhFwcnIe9YEVuAABaOpE7rV4sMBU903AL9JPmTvZ30ro00HVN8OM6p",
 
-    auth0: {
-        domain: 'dev-5tf99p7c.us.auth0.com',
-        clientId: 'GBPB42qhMWCtvrwGmYxvm5cbHXU68nzG',
-        redirectUri: 'http://localhost:4200/profile',
-    }
 };

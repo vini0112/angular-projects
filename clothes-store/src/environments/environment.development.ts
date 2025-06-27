2cfd4b4f241fee0b5b39ddc10c05f5f51506c5da
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-06-26 21:33:54 -0300

    .

--- clothes-store/src/environments/environment.development.ts
+++ clothes-store/src/environments/environment.development.ts
@@ -1,5 +1,6 @@
 export const environment = {
-    api: 'http://www.localhost:3000',
+    production:false,
+    api: "https://localhost:3000",
     stripe_public_key:"pk_test_51R9xxPBVdE5HzU7bXUGt6dXnr5V0oknWCu7ZqRdhGejhnhFwcnIe9YEVuAABaOpE7rV4sMBU903AL9JPmTvZ30ro00HVN8OM6p",
 
 };

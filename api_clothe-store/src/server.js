f2b3bbdee0340422cb18936543e696453d279bf2
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-06-27 11:28:29 -0300

    port added

--- api_clothe-store/src/server.js
+++ api_clothe-store/src/server.js
@@ -3,15 +3,16 @@ import { Server } from 'socket.io'
 import app from './app.js'
 import connection from './app/database/connection.js';
 import {setupSocket} from './app/sockets/sockets.manager.js';
+import config from './app/config/env.js';
 
 
 
 const server = http.createServer(app)
-const port = 3000
-//config.DB.port || 
+const port = config.DB.port || 3000
+
 const io = new Server(server,{
     cors: {
-        origin: [`https://vini0112.github.io`], 
+        origin: ['https://vini0112.github.io'], 
         methods: ['GET', 'POST'],
     }
 })
